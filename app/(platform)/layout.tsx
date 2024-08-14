1c143ff25691850fdfe0f0ed6d2450346917dd4a
Xiantong Wang <xtwang1996@gmail.com>
2024-08-14 15:35:05 -0400

    finish authentication and organization management

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,11 +1,7 @@
 import { ClerkProvider } from "@clerk/nextjs";
 
 const platformLayout = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <ClerkProvider>
-            {children}
-        </ClerkProvider>
-    );
+  return <ClerkProvider>{children}</ClerkProvider>;
 };
 
 export default platformLayout;
--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,34 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
+
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect();
+  }
+  if (auth().userId && isPublicRoute(request)) {
+    let path = "/select-org";
+
+    if (auth().orgId) {
+      path = `/organization/${auth().orgId}`;
+    }
+
+    const orgSelection = new URL(path, request.url);
+    return NextResponse.redirect(orgSelection);
+  }
+  if (!auth().userId && !isPublicRoute(request)) {
+    auth().redirectToSignIn({ returnBackUrl: request.url });
+  }
+  if (
+    auth().userId &&
+    !auth().orgId &&
+    request.nextUrl.pathname !== "/select-org"
+  ) {
+    const orgSelection = new URL("/select-org", request.url);
+    return NextResponse.redirect(orgSelection);
+  }
+});
 
 export const config = {
   matcher: [
