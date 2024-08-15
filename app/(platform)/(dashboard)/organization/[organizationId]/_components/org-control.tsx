3e9506cfea382be1d7cc248e299687d6dc54628e
Xiantong Wang <xtwang1996@gmail.com>
2024-08-15 00:44:11 -0400

    Start working on sidebar with workspaces(clerk organizaitons)

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/org-control.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import { useOrganizationList } from "@clerk/nextjs";
+
+export const OrgControl = () => {
+  const params = useParams();
+  const { setActive } = useOrganizationList();
+
+  useEffect(() => {
+    if (!setActive) return;
+    setActive({ organization: params.organizationId as string });
+  }, [setActive, params.organizationId]);
+  return null;
+};
