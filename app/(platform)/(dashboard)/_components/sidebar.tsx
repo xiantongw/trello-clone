46f415683f75d55ad6cfccbca125e6f485f4c427
Xiantong Wang <xtwang1996@gmail.com>
2024-08-16 00:56:00 -0400

    implemented settings page

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -47,8 +47,10 @@ export const Sidebar = ({ storageKey = "t-sidebar-state" }: SidebarProps) => {
   if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
     return (
       <>
-        {" "}
-        <Skeleton />
+        <div className="flex items-center justify-between nb-2">
+          <Skeleton className="h-10 w-[50%]" />
+          <Skeleton className="h-10 w-10" />
+        </div>
       </>
     );
   }
