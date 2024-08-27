5320969b76c8a7cf8577a4dbba5b6f4c0084ae7c
Xiantong Wang <xtwang1996@gmail.com>
2024-08-27 15:39:52 -0400

    finish server action for creating a list

---
+++ actions/create-list/schema.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const CreateList = z.object({
+  title: z
+    .string({
+      required_error: "Title is required",
+      invalid_type_error: "Title is required",
+    })
+    .min(1, {
+      message: "Title must have at least 3 letters",
+    }),
+  boardId: z.string(),
+});
