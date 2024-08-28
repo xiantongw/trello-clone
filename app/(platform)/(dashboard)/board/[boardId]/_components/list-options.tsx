66596ea34541a2120036e52e9d1c46ff6ebd7f19
Xiantong Wang <xtwang1996@gmail.com>
2024-08-28 13:03:09 -0400

    start working on list actions

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { List } from "@prisma/client";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  PopoverClose,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
+import { MoreHorizontal, X } from "lucide-react";
+import { FormSubmit } from "@/components/form/form-submit";
+import { Separator } from "@/components/ui/separator";
+
+interface ListOptionsProps {
+  data: List;
+  onAddCard: () => void;
+}
+
+export const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button className="h-auto w-autp p-2" variant="ghost">
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="px-0 pt-3 pb-3" side="bottom" align="start">
+        <div className="text-sm font-medium text-center text-neutral-600 pb-4">
+          List action
+        </div>
+        <PopoverClose asChild>
+          <Button
+            className="h-auto w-auto absolute top-2 right-2 text-neutral-600"
+            variant="ghost"
+          >
+            <X className="h-4 w-4" />
+          </Button>
+        </PopoverClose>
+        <Button
+          onClick={onAddCard}
+          className="rounded-none w-full h-auto p-2 px-5 justify-start font-normal text-sm"
+          variant="ghost"
+        >
+          Add card...
+        </Button>
+        <form>
+          <input hidden name="id" id="id" value={data.id} />
+          <input hidden name="boardId" id="boardId" value={data.boardId} />
+          <FormSubmit
+            variant="ghost"
+            className="rounded-none w-full h-auto p-2 px-5 justify-start font-normal text-sm"
+          >
+            Copy List...
+          </FormSubmit>
+        </form>
+        <Separator />
+        <form>
+          <input hidden name="id" id="id" value={data.id} />
+          <input hidden name="boardId" id="boardId" value={data.boardId} />
+          <FormSubmit
+            variant="ghost"
+            className="rounded-none w-full h-auto p-2 px-5 justify-start font-normal text-sm"
+          >
+            Delete this List...
+          </FormSubmit>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
+};
